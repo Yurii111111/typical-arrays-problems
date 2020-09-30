e4da4f4f9c364e39f6bb631382d2ee24906dbfb0
Yurii Verbitskii <km1111111@yandex.ru>
2020-09-30 15:18:55 +0300

    fix: fix task solution

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 
 exports.min = function min (array) {
   let min;
-  for (let i of array) {
-      if (i == '') {
-          min = 0;
-      } else {
-          min = Math.min.apply(null, array);
-      }
+  if ((array && array.length)) {
+    min = Math.min.apply(null, array);
+  } else {
+    return 0;
   }
   return min;
 }
 
 exports.max = function max (array) {
   let max;
-  for (let i of array) {
-      if (i == '') {
-          max = 0;
-      } else {
-          max = Math.max.apply(null, array);
-      }
+  if ((array && array.length)) {
+      max = Math.max.apply(null, array);
+  } else {
+      return 0;
   }
   return max;
 }
 
 exports.avg = function avg (array) {
-  function getCountEl(array) {
-      let numCallbackRuns = 0;
-      array.forEach((item) => numCallbackRuns++);
-      return numCallbackRuns;
-  }
+  if (array && array.length) {
 
-  function getSum(array) {
-      let sum = 0;
-      array.forEach((item) => sum += item);
-      return sum;
-  }
+    function getCountEl(array) {
+        let numCallbackRuns = 0;
+        array.forEach((item) => numCallbackRuns++);
+        return numCallbackRuns;
+    }
 
-  return getSum(array) / getCountEl(array);
+    function getSum(array) {
+        let sum = 0;
+        array.forEach((item) => sum += item);
+        return sum;
+    }
+
+    return getSum(array) / getCountEl(array);
+  } else {
+    return 0;
+  }
+  
 }
