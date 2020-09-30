09d705c4af59072d04826f47083a797f41ab8a1b
Yurii Verbitskii <km1111111@yandex.ru>
2020-09-30 13:13:18 +0300

    init: start typical-arrays-problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min;
+  for (let i of array) {
+      if (i == '') {
+          min = 0;
+      } else {
+          min = Math.min.apply(null, array);
+      }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max;
+  for (let i of array) {
+      if (i == '') {
+          max = 0;
+      } else {
+          max = Math.max.apply(null, array);
+      }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  function getCountEl(array) {
+      let numCallbackRuns = 0;
+      array.forEach((item) => numCallbackRuns++);
+      return numCallbackRuns;
+  }
+
+  function getSum(array) {
+      let sum = 0;
+      array.forEach((item) => sum += item);
+      return sum;
+  }
+
+  return getSum(array) / getCountEl(array);
 }
